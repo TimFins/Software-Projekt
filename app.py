f75ba636f0f7ed7f98b73e2756766f6705224fb3
timfinmans <tim@finmans.de>
2025-03-14 09:40:59 +0100

    Another route was added showing how the json can be accessed and send back.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'This is the service evaluating and grading graphs.'
 
+
+@app.route("/fix-tree", methods=["POST"])
+def fix_tree():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    graph = data.get("graph")
+
+    # Evaluating the graph
+
+    return jsonify({"points": 100, "feedback": "Well done!"})
 
 if __name__ == '__main__':
     app.run()
